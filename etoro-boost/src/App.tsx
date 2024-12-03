85dce3467b1f34fe49ef36921c6841d4b75fd9aa
Sarai Zohar <saraiz@etoro.com>
2024-12-03 15:49:12 +0200

    remove unused files

--- etoro-boost/src/App.tsx
+++ etoro-boost/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HeaderComponent from './components/headerComponent/Header';
 import { Login } from './components/LoginComponent/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardComponent from './components/DashboardComponent/Dashboard';
